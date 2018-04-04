7cb5283608ab0a05c1d07ab436a82d85286d8e94
CavadAhmedov <36768138+CavadAhmedov@users.noreply.github.com>
2018-04-04 19:35:17 +0400

    Add files via upload

---
+++ basics/first/js/main.js
@@ -0,0 +1,23 @@
+/*first task
+
+
+var numbers = [15 , 37 , 26 , 65 , 12 ,10];
+
+for(var i= 0 , result = 1; i < numbers.length; i++){
+	result*= numbers[i];
+}
+console.log(result);
+*/
+
+/*  second task
+var numbers = [15 , 37 , 26 , 65 , 12 ,10];
+
+for(var i= 0 , result = 0; i < numbers.length; i++){
+	result+= numbers[i]*numbers[i];
+}
+console.log(result);
+  */
+
+
+
+
